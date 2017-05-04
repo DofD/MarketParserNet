7ee9438120b1f64c2e3cee7a51701fde3a3492e4
DofD <DemonOfDarkness@mail.ru>
2017-05-04 23:08:14 +0300

    + Старт/Стоп сервиса

--- src/MarketParserNet.Console/Program.cs
+++ src/MarketParserNet.Console/Program.cs
@@ -1,6 +1,10 @@
-﻿using Castle.Core.Logging;
+﻿using System;
+
+using Castle.Core.Logging;
 using Castle.Windsor;
 using Castle.Windsor.Installer;
+
+using MarketParserNet.Framework.Interface;
 
 namespace MarketParserNet.Console
 {
@@ -16,8 +20,28 @@ namespace MarketParserNet.Console
 
             Logger = Container.Resolve<ILogger>();
 
-            Logger.Debug("Запустили парсер");
+            // Запускаем сервис
+            var service = Container.Resolve<IParserService>();
+            try
+            {
+                service.Start();
+                Logger.Debug("Запустили сервис");
+                Logger.Debug("Для выхода нажмите любую клавишу");
 
+                System.Console.ReadKey();
+            }
+            catch (Exception e)
+            {
+                // Выводим ошибку 
+                Logger.Error("Ошибка", e);
+            }
+            finally
+            {
+                service.Stop();
+                Logger.Debug("Остановили сервис");
+            }
+
+            Logger.Debug("Для выхода нажмите любую клавишу");
             System.Console.ReadKey();
         }
 
---
+++ src/MarketParserNet.Framework/Interface/IParserService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace MarketParserNet.Framework.Interface
+{
+    /// <summary>
+    ///     Сервис парсинга
+    /// </summary>
+    public interface IParserService : IDisposable
+    {
+        /// <summary>
+        ///     Запустить сервис
+        /// </summary>
+        void Start();
+
+        /// <summary>
+        ///     Остановить сервис
+        /// </summary>
+        void Stop();
+
+        /// <summary>
+        ///     Сервис остановлен
+        /// </summary>
+        bool Stopped { get; }
+    }
+}

718397b1de489330db5e2a2c708b38b325f62da9
DofD <DemonOfDarkness@mail.ru>
2017-05-04 22:42:56 +0300

    + Сборку конфигурации + Логировщик NLog

--- src/MarketParserNet.Console/Program.cs
+++ src/MarketParserNet.Console/Program.cs
@@ -1,9 +1,32 @@
-﻿namespace MarketParserNet.Console
+﻿using Castle.Core.Logging;
+using Castle.Windsor;
+using Castle.Windsor.Installer;
+
+namespace MarketParserNet.Console
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static readonly IWindsorContainer Container = new WindsorContainer();
+
+        private static ILogger Logger;
+
+        private static void Main(string[] args)
         {
+            Bootstrap();
+
+            Logger = Container.Resolve<ILogger>();
+
+            Logger.Debug("Запустили парсер");
+
+            System.Console.ReadKey();
+        }
+
+        /// <summary>
+        ///     Начальная загрузка
+        /// </summary>
+        private static void Bootstrap()
+        {
+            Container.Install(FromAssembly.This(), FromAssembly.Named("MarketParserNet.Bootstrap"));
         }
     }
 }

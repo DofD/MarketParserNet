2ec56800997b0271f8ad0520b410e968b46d9468
DofD <DemonOfDarkness@mail.ru>
2017-06-07 23:01:52 +0300

    + Регистрация кеша

---
+++ src/MarketParserNet.Bootstrap/CacheInstaller.cs
@@ -0,0 +1,40 @@
+﻿using Castle.MicroKernel.Registration;
+using Castle.MicroKernel.SubSystems.Configuration;
+using Castle.Windsor;
+
+using MarketParserNet.Domain.Impl;
+using MarketParserNet.Domain.Impl.Caches;
+using MarketParserNet.Framework.Interface;
+
+namespace MarketParserNet.Bootstrap
+{
+    public class CacheInstaller : IWindsorInstaller
+    {
+        /// <summary>
+        ///     Инсталлировать в контейнер все необходимые элементы службы
+        /// </summary>
+        /// <param name="container">Контейнер</param>
+        /// <param name="store">Хранилище конфигурации</param>
+        public void Install(IWindsorContainer container, IConfigurationStore store)
+        {
+            container.Register(
+                Component.For(typeof(ICache<,>))
+                    .ImplementedBy(typeof(CacheRedis<>))
+                    .Named("SecondCache")
+                    .LifestyleSingleton());
+
+            container.Register(
+                Component.For(typeof(ICache<,>))
+                    .ImplementedBy(typeof(CacheMemory<>))
+                    .Named("FirstCache")
+                    .LifestyleSingleton());
+
+            container.Register(
+                Component.For(typeof(ICache<,>))
+                    .ImplementedBy(typeof(CacheProvider<>))
+                    .DependsOn(Dependency.OnComponent("cacheFirst", "FirstCache"))
+                    .DependsOn(Dependency.OnComponent("cacheSecond", "SecondCache"))
+                    .LifestyleSingleton());
+        }
+    }
+}
